b41da32869fa230dab58718b344dc1ee586121aa
Mário Baptista <mariogilduarte@gmail.com>
2025-12-06 14:50:17 +0000

    changes to jenkinsfile

---
+++ CA6/Part2/gradle_transformation/src/integrationTest/java/basic_demo/AppIntegrationTest.java
@@ -0,0 +1,11 @@
+package basic_demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppIntegrationTest {
+    @Test
+    public void testAppIntegration() {
+        assertTrue(true);
+    }
+}
---
+++ CA6/Part2/gradle_transformation/src/test/java/basic_demo/AppTest.java
@@ -0,0 +1,11 @@
+package basic_demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
